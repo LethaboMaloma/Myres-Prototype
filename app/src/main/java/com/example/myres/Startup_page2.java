0eb9973cc107e8653654506f92f77d363fbe92a3
Steady-135 <2466421@students.wits.ac.za>
2023-08-26 02:32:32 +0200

    startup page

---
+++ app/src/main/java/com/example/myres/Startup_page2.java
@@ -0,0 +1,15 @@
+package com.example.myres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Startup_page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup_page2);
+    }
+}
---
+++ app/src/main/java/com/example/myres/Startup_page3.java
@@ -0,0 +1,14 @@
+package com.example.myres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Startup_page3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup_page3);
+    }
+}
