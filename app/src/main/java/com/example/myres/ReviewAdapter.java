52b4fb2959ab53389ff4dce35847ca1ba80af1ea
Steady-135 <2466421@students.wits.ac.za>
2023-09-02 09:32:18 +0200

    red adapter

--- app/src/main/java/com/example/myres/ReviewAdapter.java
+++ app/src/main/java/com/example/myres/ReviewAdapter.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder> {
     ArrayList<Review> reviewList;
